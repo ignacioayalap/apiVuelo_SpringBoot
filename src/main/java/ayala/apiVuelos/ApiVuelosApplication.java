dee199927b45d09c4708b36186522cdddcd2c904
Ignacio Ayala Poquet <ayalapoquet@gmail.com>
2026-05-21 23:17:19 -0300

    project git setup
    git push

---
+++ src/main/java/ayala/apiVuelos/ApiVuelosApplication.java
@@ -0,0 +1,13 @@
+package ayala.apiVuelos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiVuelosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiVuelosApplication.class, args);
+	}
+
+}
